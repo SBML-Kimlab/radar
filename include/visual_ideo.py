d92b3fc6d9b0024046557f3a97cff2eae509b91f
SBL-Kimlab <42198206+SBL-Kimlab@users.noreply.github.com>
2020-01-15 16:26:08 +0900

    Add files via upload

--- include/visual_ideo.py
+++ include/visual_ideo.py
@@ -180,8 +180,8 @@ class circos :
             results = "circos"
             os.rename( results + ".png", results + file_conf_0 + ".png" )
             os.rename( results + ".svg", results + file_conf_0 + ".svg" )
-            shutil.move( results + file_conf_0 + ".png", dir_vis + "output/" + file_conf_0 + ".png" )
-            shutil.move( results + file_conf_0 + ".svg", dir_vis + "output/" + file_conf_0 + ".svg" )
+            #shutil.move( results + file_conf_0 + ".png", dir_vis + "output/" + file_conf_0 + ".png" )
+            #shutil.move( results + file_conf_0 + ".svg", dir_vis + "output/" + file_conf_0 + ".svg" )
 
         #print dir_db
         #print program_dir
