360842596ce828e564f58702d9138a60bf78f24a
SBL-Kimlab <42198206+SBL-Kimlab@users.noreply.github.com>
2022-06-03 15:31:58 +0900

    Add files via upload

--- include/radar.py
+++ include/radar.py
@@ -16,18 +16,19 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 from IPython.display import clear_output
 from Bio import SeqIO
-import blast_sc, visual_ideo, circ_contents
+import blast_sc0, blast_sc1, visual_ideo, circ_contents
 from prodigal_sc import prodigal
 from db_stat import db_statistics
 
 class amr :
-	def __init__( self, strain ) :
+	def __init__( self, strain, db ) :
 		#Built-in: include, database, program, script.
 		print ( "Pipeline initiating..." ) 
 		global dir_user, main_dir, program_dir, dir_wgs, dir_genome, db_dir, dir_blast, dir_blastp, dir_result, dir_vis, dir_circos
 		global file_usearch, file_diamond, file_circos
 		self.strain = strain
-
+		self.db = db 
+		
 		dir_user = path.abspath( path.join( os.getcwd() ) )
 		os.chdir( dir_user ) 
 		#abs_piv = dir_user0.split( "/radar" )[ 0 ]
@@ -56,28 +57,31 @@ class amr :
 		def prodigal( strain ) :
 			prodigal.prodigal_sc1( strain )
 		@staticmethod
-		def db_statistics( strain ) :
-			db_statistics.db_stat( strain )
+		def db_statistics( strain, db ) :
+			db_statistics.db_stat( strain, db )
 		@staticmethod
 		class blast_method( ) :
 			def __init__( self ) :
 				pass
 			@staticmethod
-			def udb_making( strain ) :
-				call_class1_1 = blast_sc.blastp( strain ) 
-				call_class1_1.make_udb( strain )
+			def udb_making( strain, db ) :
+				call_class1_1 = blast_sc0.blastp( strain, db ) 
+				call_class1_1.make_udb( strain, db )
 			@staticmethod
-			def blastp_run( strain ) :
-				call_class1_2 = blast_sc.blastp( strain )
-				call_class1_2.blastp_run( strain )
+			def blastp_run( strain, db ) :
+				call_class1_2 = blast_sc0.blastp( strain, db )
+				call_class1_2.blastp_run( strain, db )
+		class blast_parse_method( ) :
+			def __init__( self ) :
+				pass
 			@staticmethod
-			def blastp_parse( strain ) :
-				call_class1_3 = blast_sc.blastp( strain )
-				call_class1_3.blastp_parse( strain )
+			def blastp_parse( strain, cutoff ) :
+				call_class1_3 = blast_sc1.blastp( strain, cutoff )
+				call_class1_3.blastp_parse( strain, cutoff )
 			@staticmethod
-			def blastp_merge( strain ) :
-				call_class1_4 = blast_sc.blastp( strain )
-				call_class1_4.merge_after_parse( strain )
+			def blastp_merge( strain, cutoff ) :
+				call_class1_4 = blast_sc1.blastp( strain, cutoff )
+				call_class1_4.merge_after_parse( strain, cutoff )
 		@staticmethod
 		class visual_method( ) :
 			def __init__( self ) :
